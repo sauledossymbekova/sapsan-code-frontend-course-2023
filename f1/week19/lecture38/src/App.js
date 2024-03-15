defb7bf2ce245f8b5ed13a3474567438622ff325
Saule <sauledossymbekova@gmail.com>
2024-03-15 19:09:18 +0500

    new lectures

---
+++ f1/week19/lecture38/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>F1 Hello World!!!!</h1>
+      <button>Click me!</button>
+      <button>Click me2</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ f2/week19/lecture37/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hello World!!!! </h1>
+      <p>My test projetc to Deploy</p>
+    </div>
+  );
+}
+
+export default App;
